beba60fcf219c304c1f453796ac82adf52e6c75d
ramish-khan204 <159920981+ramish-khan204@users.noreply.github.com>
2024-03-22 17:03:13 +0500

    Add files via upload

---
+++ class.ts
@@ -0,0 +1,164 @@
+//Addition
+const num1 =20;
+const num2 =30;
+console.log(num1+num2);
+
+const num3 =40;
+const num4 =10;
+const sum =num3 + num4;
+console.log(sum);
+
+const num5=20;
+const num6 =100;
+const sum2= num5 + num6;
+console.log(sum2);
+
+const Firstname ="Ramish";
+const Lastname ="Khan";
+console.log(Firstname+" "+Lastname);
+
+const variable ="pencil";
+const number ="6";
+console.log(variable + number);
+
+
+//Subtraction
+const num7 =10;
+const num8 =20;
+const sum3 =num7 - num8;
+console.log(sum3);
+
+const num9 =15;
+const num10 =25;
+console.log(num9 - num10);
+
+const num11 =20;
+const num12 =60;
+console.log(num11 - num12);
+
+const num13 =11;
+const num14 =30;
+console.log(num13 - num14);
+
+const num15 =30;
+const num16 =80;
+console.log(num15 - num16);
+
+
+//multiplication
+const num17 =10;
+const num18 =20;
+const multi =num17 * num18;
+console.log(multi);
+
+const num19 =15;
+const num20 =25;
+console.log(num19 * num20);
+
+const num21 =20;
+const num22 =60;
+console.log(num21 * num22);
+
+const num23 =11;
+const num24 =30;
+console.log(num23 * num24);
+
+const num25 =30;
+const num26 =80;
+console.log(num25 * num26);
+
+
+//Dividion
+const num27 =10
+const num28 =20;
+const divi =num27 / num28;
+console.log(divi);
+
+const num29 =15;
+const num30 =25;
+console.log(num29 / num30);
+
+const num31 =20;
+const num32 =60;
+console.log(num31 / num32);
+
+const num33 =11;
+const num34 =30;
+console.log(num33 / num34);
+
+const num35 =30;
+const num36 =80;
+console.log(num35 / num36);
+
+
+//Modulus
+const num37 =30;
+const num38 =20;
+const mod =num37 % num38;
+console.log(mod);
+
+const num39 =15;
+const num40 =25;
+const mod2 =num39 % num40
+console.log(mod2);
+
+const num41 =20;
+const num42 =60;
+const mod3 =num41 % num42
+console.log(mod3);
+
+const num43 =11;
+const num44 =30;
+const mod4 =num43 % num44
+console.log(mod4);
+
+const num45 =30;
+const num46 =80;
+const mod5 =num45 % num46
+console.log(mod5);
+
+
+//Exponent **
+const num47 =5;
+const num48 =2;
+const exp =num47 ** num48;
+console.log(exp);
+
+const num49 =2;
+const num50 =5;
+const exp2 =num49 ** num50
+console.log(exp2);
+
+const num51 =4;
+const num52 =6;
+const exp3 =num51 ** num52
+console.log(exp3);
+
+const num53 =10;
+const num54 =8;
+const exp4 =num53 ** num54
+console.log(exp4);
+
+const num55 =3;
+const num56 =8;
+const exp5 =num55 ** num56
+console.log(exp5);
+
+
+//true/false
+console.log(20==20);
+
+console.log(40===40) 
+
+//console.log(10!=2)
+
+//comparision
+console.log(10>20)
+
+console.log(10<20)
+
+console.log(10<=10)
+
+console.log(20<=50)
+
+console.log(20>=50)
